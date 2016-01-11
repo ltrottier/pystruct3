cb826d162347fbc10c0e6f445a8deca397ec6572
ltrottier <ludovic.trottier.2@gmail.com>
2016-01-11 11:56:39 -0500

    Refactoring according to Google Python Style Guide

--- List/list.py
+++ List/list.py
@@ -1,36 +1,69 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+The MIT License (MIT)
 
+Copyright (c) 2016 Ludovic Trottier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
-__author__ = "Ludovic Trottier"
-__license__ = "MIT"
-__date__ = "Sun Jan 10 12:47:32 2016"
-
 
 class List:
+
     def __init__(self):
         self._size = 0
         self._head = None
-    
+
     def insert(self, item, pos):
+        """Insert an item at a certain position.
+
+        The inserted item will be accessible at position pos. All items that
+        were formally positioned at pos, pos+1, pos+2... will be positioned at
+        pos+1, pos+2, pos+3, ...
+
+        Args:
+            item: An item.
+            pos: An integer of the position in the list.
+
+        Returns:
+            Nothing.
+
+        Raises:
+            ValueError: An error occurs if pos is out of range, i.e. if pos < 0
+                        or if pos > list size.
+        """
         raise NotImplementedError
-    
+
     def remove(self, pos):
         raise NotImplementedError
-    
+
     def read(self, pos):
         raise NotImplementedError
-    
+
     def index(self, item):
         raise NotImplementedError
-    
+
     def write(self, item, pos):
         raise NotImplementedError
-    
+
     def size(self):
         return self._size
-    
+
     def contains(self, item):
         try:
             self.index(item)
@@ -40,169 +73,172 @@ class List:
 
     def append(self, item):
         self.insert(item, self.size())
-    
+
     def pop(self):
         self.remove(self.size() - 1)
-    
+
     def __len__(self):
         return self._size
-    
-    def isEmpty(self):
+
+    def is_empty(self):
         return self._size == 0
-    
+
     def __getitem__(self, pos):
         return self.read(pos)
-        
+
     def __setitem__(self, pos, item):
         self.write(item, pos)
-    
+
     def __delitem__(self, pos):
         self.remove(pos)
-    
+
     def __contains__(self, item):
         return self.contains(item)
 
     def __iter__(self):
         return ForwardIterator(self._head)
-        
+
     def __repr__(self):
         s = "["
         for item in self:
             s = s + str(item) + ", "
-        if not self.isEmpty():
+        if not self.is_empty():
             s = s[:-2]
-        s = s + "]"           
+        s = s + "]"
         return s
 
 
 class ForwardIterator:
+
     def __init__(self, node):
         self.node = node
-    
+
     def __iter__(self):
         return self
-    
+
     def __next__(self):
-        
-        if self.node == None:
+        if self.node is None:
             raise StopIteration
-            
+
         item = self.node.item
-        self.node = self.node.nextNode
-        
+        self.node = self.node.next_node
+
         return item
-        
+
+
 class SingleLinkedList(List):
-    
+
     class Node:
-        def __init__(self, item, nextNode=None):
+
+        def __init__(self, item, next_node=None):
             self.item = item
-            self.nextNode = nextNode    
-            
+            self.next_node = next_node
+
     def __init__(self):
         List.__init__(self)
-    
+
     def insert(self, item, pos):
         if pos < 0:
             raise ValueError("Argument pos must be positive.")
-        
+
         if pos > self.size():
             raise ValueError("Argument pos must be lower than or equal to the list size.")
-        
-        if self.isEmpty():
+
+        if self.is_empty():
             self._head = SingleLinkedList.Node(item)
-        else:            
+        else:
             sentinel = self._head
             if pos == 0:
                 newNode = SingleLinkedList.Node(item, self._head)
                 self._head = newNode
-            else:                
+            else:
                 for i in range(pos-1):
-                    sentinel = sentinel.nextNode
-                
-                newNode = SingleLinkedList.Node(item, sentinel.nextNode)
-                sentinel.nextNode = newNode
-        
+                    sentinel = sentinel.next_node
+
+                newNode = SingleLinkedList.Node(item, sentinel.next_node)
+                sentinel.next_node = newNode
+
         self._size = self._size + 1
 
     def remove(self, pos):
         if pos < 0:
             raise ValueError("Argument pos must be positive.")
-        
+
         if pos >= self.size():
             raise ValueError("Argument pos must be lower than the list size.")
-        
+
         if pos == 0:
             temp = self._head
-            self._head = self._head.nextNode
-            temp.nextNode = None
+            self._head = self._head.next_node
+            temp.next_node = None
             temp = None
         else:
             sentinel = self._head
             for i in range(pos-1):
-                sentinel = sentinel.nextNode
-            temp = sentinel.nextNode
-            sentinel.nextNode = sentinel.nextNode.nextNode
-            temp.nextNode = None
+                sentinel = sentinel.next_node
+            temp = sentinel.next_node
+            sentinel.next_node = sentinel.next_node.next_node
+            temp.next_node = None
             temp = None
-    
+
         self._size = self._size - 1
-        
+
     def read(self, pos):
         if pos < 0:
             raise ValueError("Argument pos must be positive.")
-        
+
         if pos >= self.size():
             raise ValueError("Argument pos must be lower than the list size.")
-        
+
         sentinel = self._head
         for i in range(pos):
-            sentinel = sentinel.nextNode
-        
+            sentinel = sentinel.next_node
+
         return sentinel.item
 
     def index(self, item):
         sentinel = self._head
-        if self.isEmpty():
+        if self.is_empty():
             raise ValueError("Item not found.")
         i = 0
         while sentinel.item != item:
-            sentinel = sentinel.nextNode
+            sentinel = sentinel.next_node
             i = i + 1
-            if sentinel == None:
+            if sentinel is None:
                 raise ValueError("Item not found.")
-        
+
         return i
-    
+
     def write(self, item, pos):
         if pos < 0:
             raise ValueError("Argument pos must be positive.")
-        
+
         if pos >= self.size():
             raise ValueError("Argument pos must be lower than the list size.")
-        
+
         sentinel = self._head
         for i in range(pos):
-            sentinel = sentinel.nextNode
-        
+            sentinel = sentinel.next_node
+
         sentinel.item = item
-    
+
+
 class DoubleLinkedList(List):
-    
+
     class Node:
-        def __init__(self, item, nextNode):
+
+        def __init__(self, item, next_node):
             self.item = item
-            self.nextNode = nextNode
-            
+            self.next_node = next_node
+
     def __init__(self):
         List.__init__(self)
-    
+
     def insert(self, item, pos):
         print("dll")
         if pos < 0:
             raise ValueError("Argument pos must be positive.")
-        
+
         if pos > self.size():
             raise ValueError("Argument pos must be lower than the list size.")
 
-    
--- List/list_test.py
+++ List/list_test.py
@@ -1,29 +1,46 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
+The MIT License (MIT)
 
+Copyright (c) 2016 Ludovic Trottier
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
-__author__ = "Ludovic Trottier"
-__license__ = "MIT"
-__date__ = "Sun Jan 10 12:55:14 2016"
 
 import unittest
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_isEmpty(self):
+    def test_is_empty(self):
         l = List()
         self.assertEqual(l.size(), 0)
-        self.assertTrue(l.isEmpty())
+        self.assertTrue(l.is_empty())
 
     def test_insertRaises(self):
         l = List()
         with self.assertRaises(ValueError):
             l.insert(1, -1)
-        
+
         with self.assertRaises(ValueError):
             l.insert(1, 1)
-            
+
     def test_insertOk(self):
         l = List()
         l.insert(1, 0)
@@ -32,12 +49,12 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(3, l[0])
         self.assertEqual(1, l[1])
         self.assertEqual(2, l[2])
-    
+
     def test_removeRaises(self):
         l = List()
         with self.assertRaises(ValueError):
             l.remove(0)
-        
+
         l.insert(1, 0)
         l.insert(2, 1)
         l.insert(3, 0)
@@ -45,7 +62,7 @@ class TestStringMethods(unittest.TestCase):
             l.remove(-1)
         with self.assertRaises(ValueError):
             l.remove(3)
-        
+
     def test_removeOk(self):
         l = List()
         l.insert(1, 0)
@@ -55,25 +72,25 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(1, l[0])
         self.assertEqual(2, l[1])
         self.assertEqual(2, l.size())
-        l.remove(1)        
+        l.remove(1)
         self.assertEqual(1, l[0])
         self.assertEqual(1, l.size())
         l.remove(0)
         self.assertEqual(0, l.size())
-    
+
     def test_indexRaises(self):
         l = List()
         with self.assertRaises(ValueError):
             l.index(2)
-        
+
         l.insert(1, 0)
         l.insert(2, 1)
         l.insert(3, 0)
-        
+
         with self.assertRaises(ValueError):
             l.index(-3)
-        
-        
+
+
     def test_indexOk(self):
         l = List()
         l.insert(1, 0)
@@ -82,10 +99,10 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(l.index(3), 0)
         self.assertEqual(l.index(2), 2)
         self.assertEqual(l.index(1), 1)
-      
+
 if __name__ == '__main__':
     from List import SingleLinkedList as List
     unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromTestCase(TestStringMethods))
-    
+
     #from List import DoubleLinkedList as List
     #unittest.TextTestRunner().run(unittest.TestLoader().loadTestsFromTestCase(TestStringMethods))
