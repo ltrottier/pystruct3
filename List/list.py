1e826af032cb7681244d5e89bf9e794fc4b4081b
ltrottier <ludovic.trottier.2@gmail.com>
2016-01-12 12:13:11 -0500

    Added comments.

--- List/list.py
+++ List/list.py
@@ -1,70 +1,172 @@
 # -*- coding: utf-8 -*-
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2016 Ludovic Trottier
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 """
-The MIT License (MIT)
+List data structures.
 
-Copyright (c) 2016 Ludovic Trottier
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+This module contains class implementations of several List types.
 """
 
-class List:
+class List(object):
+    """List data structure interface.
+
+    This is the interface of a list data structure. All list classes are
+    sub-classes of List.
+
+    Args:
+        Nothing.
+
+    Attributes (public):
+        Nothing.
+    """
 
     def __init__(self):
+        """Create an empty list.
+        """
         self._size = 0
         self._head = None
 
-    def insert(self, item, pos):
-        """Insert an item at a certain position.
+    def insert(self, item, index):
+        """Insert an item at a certain index.
 
-        The inserted item will be accessible at position pos. All items that
-        were formally positioned at pos, pos+1, pos+2... will be positioned at
-        pos+1, pos+2, pos+3, ...
+        The inserted item will be accessible at position index. All items that
+        were formally positioned at index, index+1, index+2 ... will be
+        positioned at index+1, index+2, index+3, ...
 
         Args:
-            item: An item.
-            pos: An integer of the position in the list.
+            item (object): An item.
+            index (int): The index in the list.
 
         Returns:
             Nothing.
 
         Raises:
-            ValueError: An error occurs if pos is out of range, i.e. if pos < 0
-                        or if pos > list size.
+            ValueError: An error occurs if index is out of range, i.e.
+                        if index < 0 or if index > list size.
         """
         raise NotImplementedError
 
-    def remove(self, pos):
+    def remove(self, index):
+        """Remove the item at a certain position.
+
+        The item at position index will be removed. All items that were
+        formally positioned at index+1, index+2, index+3, ... will be
+        positioned at index, index+1, index+2, ...
+
+        Args:
+            index (int): The index in the list.
+
+        Returns:
+            Nothing.
+
+        Raises:
+            ValueError: An error occurs if index is out of range, i.e. if
+                        index < 0 or if index >= list size.
+        """
         raise NotImplementedError
 
-    def read(self, pos):
+    def read(self, index):
+        """Read the list at a certain index.
+
+        Does not modify the list.
+
+        Args:
+            index (int): The index in the list.
+
+        Returns:
+            object: The item at position index.
+
+        Raises:
+            ValueError: An error occurs if index is out of range, i.e. if
+                        index < 0 or if index >= list size.
+        """
         raise NotImplementedError
 
     def index(self, item):
+        """Return the index of the first occurence of item.
+
+        Does not modify the list.
+
+        Args:
+            item (object): The queried item.
+
+        Returns:
+            int: The index in the list.
+
+        Raises:
+            ValueError: An error occurs if item is not in the list.
+        """
         raise NotImplementedError
 
-    def write(self, item, pos):
+    def write(self, item, index):
+        """Modify the list at a certain index.
+
+        Object item will be accessible at position index.
+
+        Args:
+            item (object): The item to write.
+            index (int): The index in the list
+
+        Returns:
+            Nothing.
+
+        Raises:
+            ValueError: An error occurs if index is out of range, i.e. if
+                        index < 0 or if index >= list size.
+        """
         raise NotImplementedError
 
     def size(self):
+        """Get the size of the list.
+
+        Does not modify the list.
+
+        Args:
+            Nothing.
+
+        Returns:
+            int: The size of the list.
+
+        Raises:
+            Nothing.
+        """
         return self._size
 
     def contains(self, item):
+        """Verify if an item is in the list.
+
+        Does not modify the list.
+
+        Args:
+            item (object): The queried item.
+
+        Returns:
+            bool: True if item is in the list, False otherwise.
+
+        Raises:
+            Nothing
+        """
         try:
             self.index(item)
             return True
@@ -72,25 +174,60 @@ class List:
             return False
 
     def append(self, item):
+        """Insert an item at the end of the list.
+
+        Args:
+            item (object): The item to add.
+
+        Returns:
+            Nothing.
+
+        Raises:
+            Nothing.
+        """
         self.insert(item, self.size())
 
     def pop(self):
+        """Remove last item of the list.
+
+        Args:
+            Nothing.
+
+        Returns:
+            Nothing.
+
+        Raises:
+            ValueError: An error occurs when the list is empty.
+        """
         self.remove(self.size() - 1)
+
+    def is_empty(self):
+        """Verify if the list is empty.
+
+        Does not modify the list
+
+        Args:
+            Nothing.
+
+        Returns:
+            bool: True if the list is empty, False otherwise.
+
+        Raises:
+            Nothing.
+        """
+        return self._size == 0
 
     def __len__(self):
         return self._size
 
-    def is_empty(self):
-        return self._size == 0
+    def __getitem__(self, index):
+        return self.read(index)
 
-    def __getitem__(self, pos):
-        return self.read(pos)
+    def __setitem__(self, index, item):
+        self.write(item, index)
 
-    def __setitem__(self, pos, item):
-        self.write(item, pos)
-
-    def __delitem__(self, pos):
-        self.remove(pos)
+    def __delitem__(self, index):
+        self.remove(index)
 
     def __contains__(self, item):
         return self.contains(item)
@@ -99,16 +236,25 @@ class List:
         return ForwardIterator(self._head)
 
     def __repr__(self):
-        s = "["
+        items = ['[']
         for item in self:
-            s = s + str(item) + ", "
+            items.append(str(item))
+            items.append(', ')
         if not self.is_empty():
-            s = s[:-2]
-        s = s + "]"
-        return s
+            del items[-1]
+        items.append(']')
+        return ''.join(items)
 
 
 class ForwardIterator:
+    """Iterator for the List data structure.
+
+    Args:
+        node (Node): A linked list node.
+
+    Attributs (public):
+        node (Node): A linked list node.
+    """
 
     def __init__(self, node):
         self.node = node
@@ -127,9 +273,27 @@ class ForwardIterator:
 
 
 class SingleLinkedList(List):
+    """Single linked list data structure.
 
+    Linked list where a node contains a pointer to its adjacent neighbor.
+
+    Args:
+        Nothing.
+
+    Attributs (public):
+        Nothing.
+    """
     class Node:
+        """Node class for linked list.
 
+        Args:
+            item (object): Item contained in the node.
+            next_node (Node): Pointer to neighbor node.
+
+        Attributes (public):
+            item (object): Item contained in the node.
+            next_node (Node): Pointer to neighbor node.
+        """
         def __init__(self, item, next_node=None):
             self.item = item
             self.next_node = next_node
@@ -137,22 +301,22 @@ class SingleLinkedList(List):
     def __init__(self):
         List.__init__(self)
 
-    def insert(self, item, pos):
-        if pos < 0:
-            raise ValueError("Argument pos must be positive.")
+    def insert(self, item, index):
+        if index < 0:
+            raise ValueError('Argument index must be positive.')
 
-        if pos > self.size():
-            raise ValueError("Argument pos must be lower than or equal to the list size.")
+        if index > self.size():
+            raise ValueError('Argument index must be lower than or equal to the list size.')
 
         if self.is_empty():
             self._head = SingleLinkedList.Node(item)
         else:
             sentinel = self._head
-            if pos == 0:
+            if index == 0:
                 newNode = SingleLinkedList.Node(item, self._head)
                 self._head = newNode
             else:
-                for i in range(pos-1):
+                for i in range(index-1):
                     sentinel = sentinel.next_node
 
                 newNode = SingleLinkedList.Node(item, sentinel.next_node)
@@ -160,21 +324,21 @@ class SingleLinkedList(List):
 
         self._size = self._size + 1
 
-    def remove(self, pos):
-        if pos < 0:
-            raise ValueError("Argument pos must be positive.")
+    def remove(self, index):
+        if index < 0:
+            raise ValueError('Argument index must be positive.')
 
-        if pos >= self.size():
-            raise ValueError("Argument pos must be lower than the list size.")
+        if index >= self.size():
+            raise ValueError('Argument index must be lower than the list size.')
 
-        if pos == 0:
+        if index == 0:
             temp = self._head
             self._head = self._head.next_node
             temp.next_node = None
             temp = None
         else:
             sentinel = self._head
-            for i in range(pos-1):
+            for i in range(index-1):
                 sentinel = sentinel.next_node
             temp = sentinel.next_node
             sentinel.next_node = sentinel.next_node.next_node
@@ -183,15 +347,15 @@ class SingleLinkedList(List):
 
         self._size = self._size - 1
 
-    def read(self, pos):
-        if pos < 0:
-            raise ValueError("Argument pos must be positive.")
+    def read(self, index):
+        if index < 0:
+            raise ValueError('Argument index must be positive.')
 
-        if pos >= self.size():
-            raise ValueError("Argument pos must be lower than the list size.")
+        if index >= self.size():
+            raise ValueError('Argument index must be lower than the list size.')
 
         sentinel = self._head
-        for i in range(pos):
+        for i in range(index):
             sentinel = sentinel.next_node
 
         return sentinel.item
@@ -199,25 +363,25 @@ class SingleLinkedList(List):
     def index(self, item):
         sentinel = self._head
         if self.is_empty():
-            raise ValueError("Item not found.")
+            raise ValueError('Item not found.')
         i = 0
         while sentinel.item != item:
             sentinel = sentinel.next_node
             i = i + 1
             if sentinel is None:
-                raise ValueError("Item not found.")
+                raise ValueError('Item not found.')
 
         return i
 
-    def write(self, item, pos):
-        if pos < 0:
-            raise ValueError("Argument pos must be positive.")
+    def write(self, item, index):
+        if index < 0:
+            raise ValueError('Argument index must be positive.')
 
-        if pos >= self.size():
-            raise ValueError("Argument pos must be lower than the list size.")
+        if index >= self.size():
+            raise ValueError('Argument index must be lower than the list size.')
 
         sentinel = self._head
-        for i in range(pos):
+        for i in range(index):
             sentinel = sentinel.next_node
 
         sentinel.item = item
@@ -234,11 +398,11 @@ class DoubleLinkedList(List):
     def __init__(self):
         List.__init__(self)
 
-    def insert(self, item, pos):
-        print("dll")
-        if pos < 0:
-            raise ValueError("Argument pos must be positive.")
+    def insert(self, item, index):
+        print('dll')
+        if index < 0:
+            raise ValueError('Argument index must be positive.')
 
-        if pos > self.size():
-            raise ValueError("Argument pos must be lower than the list size.")
+        if index > self.size():
+            raise ValueError('Argument index must be lower than the list size.')
 
