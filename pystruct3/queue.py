4a5e3f6526906a1a0e2afd33fc54e59c869bbce2
ltrottier <ludovic.trottier.2@gmail.com>
2016-01-23 11:47:03 -0500

    Queue data structure

--- pystruct3/queue.py
+++ pystruct3/queue.py
@@ -23,3 +23,186 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+"""
+Queue data structures.
+
+This module contains class implementations of several Queue types.
+The available queues are:
+
+"""
+
+from pystruct3.list import DoubleLinkedList
+
+class Queue(object):
+    """Queue data structure interface.
+
+    This is the interface of a queue data structure. All queue classes are
+    sub-classes of Queue.
+
+    Args:
+        Nothing.
+
+    Attributes (public):
+        Nothing.
+    """
+    def __init__(self):
+        """Create an empty queue.
+        """
+        self.items = DoubleLinkedList()
+
+    def __del__(self):
+        """Destroy the queue.
+        """
+        self.clear()
+
+
+    def enqueue(self, item):
+        """Add an item to the queue
+
+        Args:
+            item (object): Item to add to the queue.
+
+        Return:
+            Nothing.
+
+        Raises:
+            Nothing.
+        """
+        self.items.append(item)
+
+    def dequeue(self):
+        """Remove the first item of the queue
+
+        Args:
+            Nothing.
+
+        Return:
+            object: first item of the queue
+
+        Raises:
+            ValueError: An error occurs when the list is empty.
+        """
+        return self.items.pop()
+
+    def first(self):
+        """Read the first item of the queue
+
+        Does not modify the queue.
+
+        Args:
+            Nothing.
+
+        Return:
+            object: first item of the queue
+
+        Raises:
+            ValueError: An error occurs when the list is empty.
+        """
+        return self.items[0]
+
+    def last(self):
+        """Read the last item of the queue
+
+        Does not modify the queue.
+
+        Args:
+            Nothing.
+
+        Return:
+            object: last item of the queue
+
+        Raises:
+            ValueError: An error occurs when the list is empty.
+        """
+        return self.items[-1]
+
+    def contains(self, item):
+        """Verify if an item is in the queue.
+
+        Does not modify the queue.
+
+        Args:
+            item (object): The queried item.
+
+        Returns:
+            bool: True if item is in the queue, False otherwise.
+
+        Raises:
+            Nothing
+        """
+        return (item in self.items)
+
+    def copy(self, ):
+        """Deep copy of the queue.
+
+        Args:
+            Nothing.
+
+        Returns:
+            Queue: A deep copy of the queue.
+
+        Raises:
+            Nothing.
+        """
+        return self.items.copy()
+
+    def clear(self):
+        """Remove all items from the queue.
+
+        Args:
+            Nothing.
+
+        Returns:
+            Nothing.
+
+        Raises:
+            Nothing.
+        """
+        self.items.clear()
+
+    def size(self):
+        """Get the size of the queue.
+
+        Does not modify the queue.
+
+        Args:
+            Nothing.
+
+        Returns:
+            int: The size of the queue.
+
+        Raises:
+            Nothing.
+        """
+        return self.items.size()
+
+    def is_empty(self):
+        """Verify if the queue is empty.
+
+        Does not modify the queue.
+
+        Args:
+            Nothing.
+
+        Returns:
+            bool: True if the queue is empty, False otherwise.
+
+        Raises:
+            Nothing.
+        """
+        return self.items.is_empty()
+
+    def __len__(self):
+        return self.items.size()
+
+    def __contains__(self, item):
+        return self.contains(item)
+
+    def __eq__(self, other_queue):
+        return self.items == other_queue.items
+
+    def __iter__(self):
+        return self.items.__iter__()
+
+    def __repr__(self):
+        return self.items.__repr__()
